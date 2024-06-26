13a9444ae67439e2e3637a4ea01a3a085aa53a55
Sara Saraireh <sara.sarayrah12@gmail.com>
2024-06-26 17:33:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import joblib
 import pandas as pd
+import pickle
 from sklearn.base import BaseEstimator, TransformerMixin
 
 # Define a custom transformer to map floor descriptions to numerical values
