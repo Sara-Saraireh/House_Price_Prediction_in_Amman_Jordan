b724eee2ec7f3c93e4fc7e7c1ea3c9c238e4d1d9
Sara Saraireh <sara.sarayrah12@gmail.com>
2024-06-26 19:18:25 +0300

    Update real_estate_app2.py

--- real_estate_app/real_estate_app2.py
+++ real_estate_app/real_estate_app2.py
@@ -5,8 +5,8 @@ import pickle
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.model_selection import train_test_split
-df10= pd.read_csv('real_estate_app/df10.csv')
-df9= pd.read_csv('real_estate_app/df9.csv')
+df10= pd.read_csv('/real_estate_app/df10.csv')
+df9= pd.read_csv('/real_estate_app/df9.csv')
 
 
 # Step 1: Split the Data into Training and Testing Sets
